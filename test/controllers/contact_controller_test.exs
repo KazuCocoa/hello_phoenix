4a26f544a522d8ad0158b08e3caa00ca129485ad
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-05-17 10:48:57 +0900

    add test_helper and clean some test code

--- test/controllers/contact_controller_test.exs
+++ test/controllers/contact_controller_test.exs
@@ -1,17 +1,5 @@
 defmodule HelloPhoenix.ContactControllerTest do
-  use ExUnit.Case, async: false
-  use Plug.Test
-  alias HelloPhoenix.Contact
-  alias HelloPhoenix.Repo
-  alias Ecto.Adapters.SQL
-
-  setup do
-    SQL.begin_test_transaction(Repo)
-
-    on_exit fn ->
-      SQL.rollback_test_transaction(Repo)
-    end
-  end
+  use HelloPhoenix.Case, async: false
 
   test "/index returns a list of contacts" do
     contacts_as_json =
@@ -25,11 +13,4 @@ defmodule HelloPhoenix.ContactControllerTest do
     assert response.status == 200
     assert response.resp_body == contacts_as_json 
   end
-
-  defp send_request(conn) do
-    conn
-    |> put_private(:plug_skip_csrf_protection, true)
-    |> HelloPhoenix.Endpoint.call([])
-  end
-
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,33 @@
+defmodule HelloPhoenix.Case do
+  use ExUnit.CaseTemplate
+  alias Ecto.Adapters.SQL
+  alias HelloPhoenix.Repo
+ 
+  setup do
+    SQL.begin_test_transaction(Repo)
+ 
+    on_exit fn ->
+      SQL.rollback_test_transaction(Repo)
+    end
+  end
+ 
+  using do
+    quote do
+      alias HelloPhoenix.Repo
+      alias HelloPhoenix.Contact
+      use Plug.Test
+ 
+      # Remember to change this from `defp` to `def` or it can't be used in your
+      # tests.
+      def send_request(conn) do
+        conn
+        |> put_private(:plug_skip_csrf_protection, true)
+        |> HelloPhoenix.Endpoint.call([])
+      end
+    end
+  end
+end
+
 ExUnit.start
 
 # Create the database, run migrations, and start the test transaction.
